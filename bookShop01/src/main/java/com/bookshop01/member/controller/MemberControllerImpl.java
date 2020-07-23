f27bfc944cd919eb1449bb0642e10bffe4df641e
joup23 <64519908+joup23@users.noreply.github.com>
2020-07-23 16:21:24 +0900

    아이디중복 ajax

--- bookShop01/src/main/java/com/bookshop01/member/controller/MemberControllerImpl.java
+++ bookShop01/src/main/java/com/bookshop01/member/controller/MemberControllerImpl.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.aspectj.org.eclipse.jdt.internal.codeassist.impl.Keywords;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,69 +20,70 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bookshop01.common.base.BaseController;
 import com.bookshop01.member.service.MemberService;
 import com.bookshop01.member.vo.MemberVO;
 
+import net.sf.json.JSONObject;
+
 @Controller("memberController")
-@RequestMapping(value="/member")
-public class MemberControllerImpl extends BaseController implements MemberController{
+@RequestMapping(value = "/member")
+public class MemberControllerImpl extends BaseController implements MemberController {
 	@Autowired
 	private MemberService memberService;
 	@Autowired
 	private MemberVO memberVO;
 	@Autowired
 	BCryptPasswordEncoder passEncoder;
-	
+
 	@Override
-	@RequestMapping(value="/login.do" ,method = RequestMethod.POST)
-	public ModelAndView login(@RequestParam Map<String, String> loginMap,
-			                  HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
+	public ModelAndView login(@RequestParam Map<String, String> loginMap, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		ModelAndView mav = new ModelAndView();
-		 memberVO=memberService.login(loginMap);
-		 String passwd = loginMap.get("member_pw");
-		 boolean passMath = passEncoder.matches(passwd, memberVO.getMember_pw());
-		 
-		if(memberVO!= null && passMath){
-			HttpSession session=request.getSession();
-			session=request.getSession();
+		memberVO = memberService.login(loginMap);
+		String passwd = loginMap.get("member_pw");
+		boolean passMath = passEncoder.matches(passwd, memberVO.getMember_pw());
+
+		if (memberVO != null && passMath) {
+			HttpSession session = request.getSession();
+			session = request.getSession();
 			session.setAttribute("isLogOn", true);
-			session.setAttribute("memberInfo",memberVO);
-			
-			String action=(String)session.getAttribute("action");
-			if(action!=null && action.equals("/order/orderEachGoods.do")){
-				mav.setViewName("forward:"+action);
-			}else{
-				mav.setViewName("redirect:/main/main.do");	
+			session.setAttribute("memberInfo", memberVO);
+
+			String action = (String) session.getAttribute("action");
+			if (action != null && action.equals("/order/orderEachGoods.do")) {
+				mav.setViewName("forward:" + action);
+			} else {
+				mav.setViewName("redirect:/main/main.do");
 			}
-			
-			
-			
-		}else{
-			String message="아이디나  비밀번호가 틀립니다. 다시 로그인해주세요!!!";
+
+		} else {
+			String message = "아이디나  비밀번호가 틀립니다. 다시 로그인해주세요!!!";
 			mav.addObject("message", message);
 			mav.setViewName("/member/loginForm");
 		}
 		return mav;
 	}
-	
+
 	@Override
-	@RequestMapping(value="/logout.do" ,method = RequestMethod.GET)
+	@RequestMapping(value = "/logout.do", method = RequestMethod.GET)
 	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ModelAndView mav = new ModelAndView();
-		HttpSession session=request.getSession();
+		HttpSession session = request.getSession();
 		session.setAttribute("isLogOn", false);
 		session.removeAttribute("memberInfo");
 		mav.setViewName("redirect:/main/main.do");
 		return mav;
 	}
-	
+
 	@Override
-	@RequestMapping(value="/addMember.do" ,method = RequestMethod.POST)
-	public ResponseEntity addMember(@ModelAttribute("memberVO") MemberVO _memberVO,
-			                HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@RequestMapping(value = "/addMember.do", method = RequestMethod.POST)
+	public ResponseEntity addMember(@ModelAttribute("memberVO") MemberVO _memberVO, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("utf-8");
 		String message = null;
@@ -90,31 +92,33 @@ public class MemberControllerImpl extends BaseController implements MemberContro
 		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
 		try {
 			String inputPass = _memberVO.getMember_pw();
-			String pass=passEncoder.encode(inputPass);
+			String pass = passEncoder.encode(inputPass);
 			_memberVO.setMember_pw(pass);
-		    memberService.addMember(_memberVO);
-		    message  = "<script>";
-		    message +=" alert('회원 가입을 마쳤습니다.로그인창으로 이동합니다.');";
-		    message += " location.href='"+request.getContextPath()+"/member/loginForm.do';";
-		    message += " </script>";
-		    
-		}catch(Exception e) {
-			message  = "<script>";
-		    message +=" alert('작업 중 오류가 발생했습니다. 다시 시도해 주세요');";
-		    message += " location.href='"+request.getContextPath()+"/member/memberForm.do';";
-		    message += " </script>";
+			memberService.addMember(_memberVO);
+			message = "<script>";
+			message += " alert('회원 가입을 마쳤습니다.로그인창으로 이동합니다.');";
+			message += " location.href='" + request.getContextPath() + "/member/loginForm.do';";
+			message += " </script>";
+
+		} catch (Exception e) {
+			message = "<script>";
+			message += " alert('작업 중 오류가 발생했습니다. 다시 시도해 주세요');";
+			message += " location.href='" + request.getContextPath() + "/member/memberForm.do';";
+			message += " </script>";
 			e.printStackTrace();
 		}
-		resEntity =new ResponseEntity(message, responseHeaders, HttpStatus.OK);
+		resEntity = new ResponseEntity(message, responseHeaders, HttpStatus.OK);
 		return resEntity;
 	}
-	
+
 	@Override
-	@RequestMapping(value="/overlapped.do" ,method = RequestMethod.POST)
-	public ResponseEntity overlapped(@RequestParam("id") String id,HttpServletRequest request, HttpServletResponse response) throws Exception{
+	@RequestMapping(value = "/overlapped.do", method = RequestMethod.POST)
+	public ResponseEntity overlapped(@RequestParam("id") String id, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		ResponseEntity resEntity = null;
 		String result = memberService.overlapped(id);
-		resEntity =new ResponseEntity(result, HttpStatus.OK);
+		resEntity = new ResponseEntity(result, HttpStatus.OK);
 		return resEntity;
 	}
+
 }
